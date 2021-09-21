78703983e5debe587b030adb4a6d89e6d745d41d
shubham171019 <41218386+shubham171019@users.noreply.github.com>
2021-09-21 16:46:08 +0530

    Update assg4.java

--- java/assg4.java
+++ java/assg4.java
@@ -1,13 +1,13 @@
 import java.util.*;
 class main{
-    Public static void main(String[] args){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the series");
-        int num = sc.newInt();
-        int a []= new int[num];
+        int num = sc.nextInt();
+        long a []= new long[num];
         a[0]=0;
         a[1]=1;
-        for(int i=0;i<num;i++){
+        for(int i=2;i<num;i++){
             a[i] = a[i - 2] + a[i - 1];
         }
         for (int i = num - 1; i >= 0; i--)
