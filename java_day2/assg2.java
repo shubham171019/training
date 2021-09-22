58d5df562e3473dc8e0778aff102fe3ab153e45c
shubham171019 <41218386+shubham171019@users.noreply.github.com>
2021-09-22 10:32:52 +0530

    Update assg2.java

--- java_day2/assg2.java
+++ java_day2/assg2.java
@@ -26,24 +26,20 @@ class main{
         for (int i=1; i<n; i++)
         {
  
-            // inner loop to handle number spaces
-            // values changing acc. to requirement
             for (int j=n-i; j>1; j--)
             {
-                // printing spaces
+                
                 System.out.print(" ");
             }
   
-            //  inner loop to handle number of columns
-            //  values changing acc. to outer loop
             for (int j=0; j<=i; j++ )
             {
-                // printing stars
+              
                 System.out.print(result+" ");
                 sum = sum+ result;
             }
             result = sum;
-            // ending line after each row
+        
             System.out.println();
         }
     }
